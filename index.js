8054c81143e78aae78981341b4704b8703d2af6f
Antti Raita <n1472@student.jamk.fi>
2023-10-18 12:58:07 +0300

    fullstack sovellus

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
